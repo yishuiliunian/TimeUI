0559fadefeffae2dee96c515432221fc829eee47
Stone Dong <stonedong@tencent.com>
2013-12-27 11:04:08 +0800

    no message

--- scripts/update-bundle.sh
+++ scripts/update-bundle.sh
@@ -1,7 +1,3 @@
-if [ ! -z "$INFO_PLIST" ]; then
-  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$INFO_PLIST"
-  echo "Set CFBundleVersion to $TRAVIS_BUILD_NUMBER"
-fi
 
 if [ ! -z "$BUNDLE_IDENTIFIER" ]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_IDENTIFIER" "$INFO_PLIST"
