b6f2ed2f47f458586907b52427b04f5a1d783f60
Stone Dong <stonedong@tencent.com>
2014-01-24 15:05:45 +0800

    摇晃换出 全局菜单

--- TimeUI/DZMainViewController.m
+++ TimeUI/DZMainViewController.m
@@ -9,12 +9,25 @@
 #import "DZMainViewController.h"
 #import "DZAccountManager.h"
 #import "DZSeparationLine.h"
-@interface DZMainViewController ()
+#import "DZGlobalActionView.h"
+#import "DZNotificationCenter.h"
+#import "DZShakeRecognizedWindow.h"
+#import "DZSyncActionItemView.h"
+#import "DZLabelActionItem.h"
+#import "DZHistoryViewController.h"
+#import "DZSettingsViewController.h"
+#import "DZLoginViewController.h"
+#import "DZRegisterViewController.h"
+@interface DZMainViewController () <DZShareInterface,DZActionDelegate>
 
 @end
 
 @implementation DZMainViewController
 
+- (void) dealloc
+{
+    [[DZNotificationCenter defaultCenter] removeObserver:self forMessage:DZShareNotificationMessage];
+}
 - (DZAccount*)account
 {
     return DZActiveAccount;
@@ -31,7 +44,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [[DZNotificationCenter defaultCenter] addObserver:self forKey:DZShareNotificationMessage];
     [_typesViewController willMoveToParentViewController:self];
     [self addChildViewController:_typesViewController];
     [self.view addSubview:_typesViewController.view];
@@ -65,5 +78,77 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) didGetShareMessage
+{
+    DZGlobalActionView* actionView = [[DZGlobalActionView alloc] init];
+    actionView.delegate = self;
+    DZSyncActionItemView* syncItem = [[DZSyncActionItemView alloc] init];
+    syncItem.height = 40;
+    syncItem.backgroundColor = [UIColor redColor];
+    
+    
+    DZLabelActionItem* historyItem = [[DZLabelActionItem alloc] init];
+    historyItem.textLabel.text = @"历史记录";
+    historyItem.height = 70;
+    
+    DZLabelActionItem* settingItem = [[DZLabelActionItem alloc] init];
+    settingItem.textLabel.text = @"设置";
+    settingItem.height = 70;
+    
+    DZLabelActionItem* cancelItem = [[DZLabelActionItem alloc] init];
+    cancelItem.textLabel.text = @"取消";
+    cancelItem.height = 70;
+    
+    DZLabelActionItem* loginItem = [[DZLabelActionItem alloc] init];
+    loginItem.textLabel.text = @"登陆";
+    loginItem.height = 70;
+    
+    DZLabelActionItem* registerItem = [[DZLabelActionItem alloc] init];
+    registerItem.textLabel.text = @"注册";
+    registerItem.height = 70;
+    
+    cancelItem.textLabel.textAlignment = NSTextAlignmentRight;
+    [actionView.actionContentView setItems:@[syncItem, historyItem, settingItem, loginItem , registerItem,cancelItem]];
+    [actionView showWithAnimation:YES];
+}
+
+- (BOOL) actionView:(DZActionView *)actionView shouldHideTapAtIndex:(NSInteger)index item:(DZActionItemView *)item
+{
+    if (index == 0) {
+        return NO;
+    }
+    return YES;
+}
+
+- (void) actionView:(DZActionView *)actionView didHideWithTapAtIndex:(NSInteger)index item:(DZActionItemView *)item
+{
+    if (index == 1) {
+        UINavigationController* navigationVC = [[UINavigationController alloc] initWithRootViewController:[DZHistoryViewController new]];
+        [self presentViewController:navigationVC animated:YES completion:^{
+            
+        }];
+    } else if (index == 2)
+    {
+        UINavigationController* navigationVC = [[UINavigationController alloc] initWithRootViewController:[DZSettingsViewController new]];
+        [self presentViewController:navigationVC animated:YES completion:^{
+            
+        }];
+    } else if (index == 3) {
+        UINavigationController* navVC = [[UINavigationController alloc] initWithRootViewController:[DZLoginViewController new]];
+        __unused id a = navVC.view;
+        [self presentViewController:navVC animated:YES completion:^{
+            
+        }];
+    } else if (index == 4)
+    {
+        UINavigationController* navVC = [[UINavigationController alloc] initWithRootViewController:[DZRegisterViewController new]];
+        __unused id a = navVC.view;
+        [self presentViewController:navVC animated:YES completion:^{
+            
+        }];
+    }
+}
+
 
 @end
